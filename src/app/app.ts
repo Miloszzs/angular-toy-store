4da3ca11b9277fc4352696f3714a845599651e24
Miloš Stojanović <mstojanovic1811@gmail.com>
2026-03-10 22:54:32 +0100

    Add angular material and basic structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
-import { Component, signal } from "@angular/core";
+import { Component } from "@angular/core";
 import { RouterOutlet, RouterLinkWithHref } from "@angular/router";
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref],
+  imports: [RouterOutlet, RouterLinkWithHref, MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
-  protected readonly title = signal('proba proba')
+  
 }
