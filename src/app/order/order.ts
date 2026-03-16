3da58c7500aad7464b8bbf26d9309405e8b43b6f
Miloš Stojanović <mstojanovic1811@gmail.com>
2026-03-16 19:29:47 +0100

    Added order tab

---
+++ src/app/order/order.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Toy } from '../../models/toy.model';
+
+@Component({
+  selector: 'app-order',
+  imports: [],
+  templateUrl: './order.html',
+  styleUrl: './order.css',
+})
+export class Order {
+  
+}
